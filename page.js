06e8e8694a67b1e4ba882c0df0d78367001ff40a
kfedus6 <feduskolya@gmail.com>
2022-01-17 13:03:33 +0200

    Sample code

--- page.js
+++ page.js
@@ -37,7 +37,7 @@ $('li:last').css('background', "green");
 //$('.test li:odd:last').css('color', 'green')
 
 //DZ
-
+/*
 $('#age-table').css('background', 'pink');
 $('#age-table label').css('color', 'red');
 $('#age-table td:first').css('font-size', '40px');
@@ -46,3 +46,77 @@ $('[name="search"]').css('background', 'orange');
 $('[name="search"]').css('font-size', '30px');
 $('[name="search"] input:first').css('background', 'blue');
 $('[name="search"] input:last').css('background', 'yellow');
+ */
+
+
+
+//$('ul li:gt(3)').css('background', 'red');
+//$('ul li:lt(3)').css('background', 'green');
+//$('ul li:gt(4):even').css('background', 'red');
+//$('ul li:lt(5):odd').css('background', 'yellow');
+
+//$('*:header').css('color', 'green')
+//$('*:not()').css('color', 'red')
+
+//$('div:contains("")').css('color', 'red')
+
+/*
+$(document).ready(() => {
+   let checkd = true;
+   $('button').click(() => {
+      console.log('click')
+      if (checkd == true) {
+         $('#test').hide(2000)
+         checkd = false;
+      } else if (checkd == false) {
+         $('#test').show(2000);
+         checkd = true;
+      }
+   })
+})
+*/
+
+/*
+const arr = $('tabl').filter("tr")
+arr.css('color', 'red');
+*/
+
+/*
+const lis = $('ul li');
+lis.eq(4).css('color', 'blue')
+lis.first().css('color', 'red')
+lis.last().css('color', 'green')
+*
+
+/*
+for (let i = 0; i < lis.length; i++) {
+   console.log(lis[i])
+}
+*/
+/*
+lis.each((idx, item) => {
+   console.log(item)
+})
+*/
+
+/*
+$('button').click(() => {
+   $('ul li:even').hide();
+})
+*/
+/* 
+const lis = $('ul li');
+const li = lis.last()
+idxLi = lis.index(li)
+console.log(idxLi) */
+/* 
+const lis = $('ul li');
+const lis_two = $('.test')
+lis.each((idx, item) => {
+   lis_two.each((idx_two, item_two) => {
+      if (item == item_two) {
+         console.log(idx)
+      }
+   })
+}) 
+*/
